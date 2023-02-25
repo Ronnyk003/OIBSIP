a2cc6b180b2daf7ea201282b61b33743c20d2bf9
Ronnyk003 <125789830+Ronnyk003@users.noreply.github.com>
2023-02-25 20:18:18 +0530

    Add files via upload

---
+++ src/onlinereservationsystem/Main.java
@@ -0,0 +1,8 @@
+package onlinereservationsystem;
+
+import java.lang.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Login login = new Login();
+    }
+}
